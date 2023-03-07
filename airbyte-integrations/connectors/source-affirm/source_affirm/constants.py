9fb96367a237e7db92695d213693b264023765f9
Terry Yin <terry.yin@onepeloton.com>
2023-03-07 16:26:44 -0500

    change constants

--- airbyte-integrations/connectors/source-affirm/source_affirm/constants.py
+++ airbyte-integrations/connectors/source-affirm/source_affirm/constants.py
@@ -21,19 +21,10 @@ class AffirmSettlementReportType(str, Enum):
     DETAILS = "details"
 
 
-def get_affirm_settlement_base_url(affirm_country: AffirmCountry, report_type: AffirmSettlementReportType) -> str:
+def get_affirm_settlement_base_url(affirm_country: AffirmCountry) -> str:
     base_urls = {
-        AffirmCountry.US: {
-            AffirmSettlementReportType.DETAILS: "https://api.affirm.com/api/v1/settlements/events",
-            AffirmSettlementReportType.SUMMARY: "https://api.affirm.com/api/v1/settlements/daily",
-        },
-        AffirmCountry.CA: {
-            AffirmSettlementReportType.DETAILS: "https://api.affirm.ca/api/v1/settlements/events",
-            AffirmSettlementReportType.SUMMARY: "https://api.affirm.ca/api/v1/settlements/daily",
-        },
-        AffirmCountry.AU: {
-            AffirmSettlementReportType.DETAILS: "https://au.affirm.com/api/v1/settlements/events",
-            AffirmSettlementReportType.SUMMARY: "https://au.affirm.com/api/v1/settlements/daily"
-        }
+        AffirmCountry.US: "https://api.affirm.com/api/v1",
+        AffirmCountry.CA: "https://api.affirm.ca/api/v1",
+        AffirmCountry.AU: "https://au.affirm.com/api/v1"
     }
-    return base_urls[affirm_country][report_type]
+    return base_urls[affirm_country]
